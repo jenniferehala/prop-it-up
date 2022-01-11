e5abbddc7634f9104f17425302e4080879608906
jehala <jenniferehala@github.com>
2022-01-11 15:21:17 -0500

    commit

--- src/components/personcard.js
+++ src/components/personcard.js
@@ -1,6 +1,17 @@
 import React, { Component } from 'react';
 
 class PersonCard extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            age: this.props.age
+        };
+    }
+
+    birthday = (e) => {
+        this.setState( {age: this.state.age + 1})
+    }
+
     render() {
         return <div>
                     <div>
@@ -8,9 +19,9 @@ class PersonCard extends Component {
                     </div>
 
                     <h1>{this.props.lastName} , {this.props.firstName}</h1>
-                    <p>Age:{this.props.age}</p>
+                    <p>Age:{this.state.age}</p>
                     <p>Hair Color:{this.props.hairColor}</p> 
-
+                    <button onClick= {this.birthday}>Birthday Button for {this.props.firstName}</button>
                 </div>
     }
 }
